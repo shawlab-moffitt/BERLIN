b1de3eb207d4001b73d3869b8c97fb5e3d61aa6e
aobermayer4 <aobermayer4@gmail.com>
2024-10-15 15:20:38 -0400

    Move dimension reduction and clustering to separate function

--- R/berlin_qc.R
+++ R/berlin_qc.R
@@ -119,22 +119,16 @@ berlin_filter <- function(seurat = NULL, species = "human", percent_mt = NULL, m
 #' @param logScale Numeric. Set the scale to log normalize data. Default to 10000.
 #' @param remove_duplicates Boolean. To remove duplicate features that may be found in the data, use TRUE, else FALSE and the function will notify you if duplicate features are found.
 #' @param seed Numeric. Random seed, default is 42. Setting to NULL will remove seed.
-#' @param resolution Numeric vector. Above one to obtain a larger number of communities, and below one to obtain a smaller number.
-#' @param pca_npcs Numeric. Total number of principal components to compute for PCA plot. Default is 30.
-#' @param tsne_dims Numeric sequence. Dimensions to use as input features for TSNE plot. Default 1:30.
-#' @param umap_dims  Numeric sequence. Dimensions to use as input features for UMAP. Default 1:10.
-#' @param neighbor_dims Numeric sequence. Dimension of reduction to use as input to find neighbors.
 #' @param doublet_pN Numeric. The number of generated artificial doublets, expressed as a proportion of the merged real-artificial data. Default is set to 0.25.
 #' @param doublet_pK Numeric. The principal component neighborhood size to compute pANN, expressed as a proportion of the merged real-artificial data. Default is 0.09.
 #' @param doublet_prop Numeric. Estimated proportion of homotypic doublets. Default is 0.04.
 #' @param doublet_PCs Numeric. Number of statistically-significant principal components.
 #'
-#' @return Seurat object.
+#' @return A Seurat object.
 #' @export
 #'
 berlin_qc <- function(object = NULL, counts = NULL, meta = NULL, assay = "RNA", project_name = "BERLIN_Project", species = "human", seed = 42,
                       percent_mt = NULL, minFeatures = 500, varFeatures = 2000, verbose = TRUE, logScale = 10000, remove_duplicates = TRUE,
-                      resolution = c(0.5,1,1.5,2), pca_npcs = 30, tsne_dims = 1:30, umap_dims = 1:10, neighbor_dims = 1:30,
                       doublet_pN = 0.25, doublet_pK = 0.09, doublet_prop = 0.04, doublet_PCs = 1:10) {
 
 
@@ -211,24 +205,6 @@ berlin_qc <- function(object = NULL, counts = NULL, meta = NULL, assay = "RNA", 
     message("Scaling data")
   }
   object <- Seurat::ScaleData(object, vars.to.regress = c("nFeature_RNA", "percent.mt"), verbose = verbose)
-
-  # Perform dimensionality reduction and clustering
-  if (verbose) {
-    message("Dimension reduciton")
-  }
-  object <- Seurat::RunPCA(object, npcs = pca_npcs, verbose = verbose, seed.use = seed)
-  object <- Seurat::RunTSNE(object, reduction = "pca", dims = tsne_dims, seed.use = seed)
-  object <- Seurat::RunUMAP(object, dims = umap_dims, verbose = verbose, seed.use = seed)
-  object <- Seurat::FindNeighbors(object, reduction = "pca", dims = neighbor_dims, verbose = verbose)
-  object <- Seurat::FindClusters(object, resolution = resolution, verbose = verbose)
-
-  # Add UMAP coordinates to the metadata
-  UMAP <- as.data.frame(Embeddings(object = object[["umap"]]))
-  object <- Seurat::AddMetaData(object,metadata = UMAP)
-  # Add tSNE coordinates to the metadata
-  tsne <- as.data.frame(Embeddings(object = object[["tsne"]]))
-  object <- Seurat::AddMetaData(object,metadata = tsne)
-
 
   # Find doublets using DoubletFinder
   if (verbose) {

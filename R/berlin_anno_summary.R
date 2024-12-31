c03412edd252a08abf826ffaf8f555d94a057d4d
aobermayer4 <aobermayer4@gmail.com>
2024-12-31 18:23:02 -0500

    add summary annotation funciton

---
+++ R/berlin_anno_summary.R
@@ -0,0 +1,65 @@
+
+
+#' Write out BERLIN Annotation Summary
+#'
+#' @param object Seurat object. If NULL, must supply meta data.
+#' @param meta A data.frame. The rows are barcode/cell names, and the column are any additionally supplied metadata. Row names in the metadata need to match the column names of the counts matrix.
+#' @param file String. Desired file name with directory for output.
+#' @param excel Boolean. If TRUE, file will be save as excel workbook file.
+#' @param anno_cols Character Vector. Vector of column names to be subset and written out to file. List of default columns in details.
+#'
+#' @return data frame
+#' @export
+#'
+#' @details
+#' Default annotation columns: c("scType_seurat_clusters_Classification","scType Score Sum","ncells","scType_Cell_Classification",
+#' "scGate_Myeloid_Macrohphage_Celltype","seurat_clusters","RNA_snn_res.0.5","RNA_snn_res.1",
+#' "RNA_snn_res.1.5","RNA_snn_res.2","dice.main","dice.fine", "monaco.main","monaco.fine",
+#' "northern.main","northern.fine","blue.main","blue.fine")
+#'
+#'
+
+
+
+
+berlin_anno_summary <- function(object = NULL, meta = NULL, file = NULL, excel = TRUE,
+                                anno_cols = c("^scType_.*\\_Classification$","scType Score Sum","ncells","scType_Cell_Classification",
+                                              "^scGate_.*\\_Celltype$","seurat_clusters","RNA_snn_res.0.5","RNA_snn_res.1",
+                                              "RNA_snn_res.1.5","RNA_snn_res.2","dice.main","dice.fine", "monaco.main","monaco.fine",
+                                              "northern.main","northern.fine","blue.main","blue.fine")) {
+
+  if (is.null(object) & is.null(meta)) stop("Please supply Seurat object or meta data")
+
+  if (is.null(meta)) {
+    meta <- object[[]]
+  }
+
+  cols_found <- grep(paste(anno_cols,collapse = "|"), colnames(meta), value = T, ignore.case = T)
+
+  meta_anno <- meta[,cols_found]
+  scGate_col <- grep("^scGate_.*\\_Celltype$", colnames(meta_anno), value = T)
+  scGate_col <- grep("multiscGate",scGate_col, invert = T, value = T)
+  scType_col <- grep("^scType_.*\\_Classification$", colnames(meta_anno), value = T)
+  scType_col <- grep("scType_Cell_Classification",scType_col,invert = T, value = T)
+
+  meta_anno <- meta_anno %>%
+    relocate(any_of(c(scType_col,"scType Score Sum","ncells","scType_Cell_Classification",scGate_col,"seurat_clusters")))
+  meta_anno <- cbind(cell = rownames(meta_anno),
+                     meta_anno)
+
+
+  if (excel) {
+    if (is.null(file)) {
+      file <- paste0(getwd(),"/BERLIN_Annotation_Summary_",Sys.Date(),".xlsx")
+    }
+    writexl::write_xlsx(meta_anno, file)
+  } else {
+    if (is.null(file)) {
+      file <- paste0(getwd(),"/BERLIN_Annotation_Summary_",Sys.Date(),".txt")
+    }
+    write.table(meta_anno,file, sep = '\t', row.names = F)
+  }
+
+  return(meta_anno)
+
+}
